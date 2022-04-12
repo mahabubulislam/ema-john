891109340b2a82f4aa0b947044fcba580594e0e4
mahbub-webdev <mahabub2k1@gmail.com>
2022-04-12 23:28:53 +0600

    google btn and sign up component  added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
+import google from '../../images/google.png'
 const Login = () => {
     return (
         <div className='form-container'>
@@ -9,17 +10,23 @@ const Login = () => {
                 <form>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input type="email" name="email" id="" required/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input type="password" name="password" id="" required/>
                     </div>
                     <input className='form-submit' type="submit" value="Login" />
                 </form>
                 <p>
                     New to Ema-John? <Link className='form-link' to="/signup">Create an account</Link>
                 </p>
+                <div className='border-line'>
+                    <div className='line'></div>
+                    <p>or</p>
+                    <div className='line'></div>
+                </div>
+                <button className='btn-google'><img src={google} alt="" />Continue with Google</button>
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import google from '../../images/google.png'
 
 const SignUp = () => {
     return (
+        <div className='form-container'>
         <div>
-            
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Confirm Password</label>
+                    <input type="password" name="confirm password" id="" required/>
+                </div>
+                <input className='form-submit' type="submit" value="Login" />
+            </form>
+            <p>
+                Already have a account? <Link className='form-link' to="/login">Login</Link>
+            </p>
+            <div className='border-line'>
+                <div className='line'></div>
+                <p>or</p>
+                <div className='line'></div>
+            </div>
+            <button className='btn-google'><img src={google} alt="" />Continue with Google</button>
         </div>
+    </div>
     );
 };
 
