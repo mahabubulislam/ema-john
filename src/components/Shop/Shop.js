8db1b236a8a44534cc80632912169eb863da4ae3
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-09 16:55:20 +0600

    data loaded from mongodb server

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,25 +13,25 @@ const Shop = () => {
 
     const handleAddToCart = (selectedProduct) => {
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
+        const exists = cart.find(product => product._id === selectedProduct._id);
         if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
         else {
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
         setCart(newCart);
-        addToDb(selectedProduct.id)
+        addToDb(selectedProduct._id)
     }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
                     products.map(product => <Product
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                     ></Product>)
